9adde05ea9ad4635da96c46702426c90935e6a9d
Cyanoure <kozelka.r@gmail.com>
2023-03-30 20:01:45 +0200

    More search strings

--- filter.js
+++ filter.js
@@ -3,9 +3,15 @@ const readline = require('readline');
 
 // Get command-line arguments
 const args = process.argv.slice(2);
+if(args.length < 2) {
+  console.error("Minimum 2 arguments required. Input file name, and the words to search.");
+  console.error("Example: node filter.js myfile.csv Project1 Project2 \"Project with spaces\"");
+  process.exit();
+}
 const filename = args[0];
-const searchString = args[1].toLowerCase();
 const outputFile = filename.replace('.csv', '_filtered.csv');
+
+const searchStrings = args.slice(1, args.length);
 
 // Save user from losing the original file when replace failed and outputFile == input file
 if (filename == outputFile) {
@@ -26,8 +32,17 @@ rl.on('line', (line) => {
   // Convert the line to lowercase before checking for a match
   const lowerCaseLine = line.toLowerCase();
 
-  // Check if the line contains the search string
-  if (lowerCaseLine.includes(searchString)) {
+  // Check if the line contains one of the search strings
+  let foundAny = false;
+  for(let i = 0; i < searchStrings.length; i++){
+    const searchString = searchStrings[i].toLowerCase();
+    if(lowerCaseLine.includes(searchString)) {
+      foundAny = true;
+      break;
+    }
+  }
+
+  if (foundAny) {
     // Write the line to the output file
     ws.write(line + '\n');
   }
